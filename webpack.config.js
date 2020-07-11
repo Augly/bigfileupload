3a772275ad1140fa498f25e08bb0e3d9ca705d9b
zero <1137774080@qq.com>
2020-07-12 03:55:13 +0800

    初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/*
+ * @Descripttion : 
+ * @version      : 
+ * @Author       : zero
+ * @Date         : 2020-07-11 13:14:22
+ * @LastEditors  : zero
+ * @LastEditTime : 2020-07-12 03:43:54
+ */ 
+ const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
+module.exports = {
+  //文件入口
+  entry: {
+    index: './index.js'
+  },
+  mode: 'production',
+  plugins: [new CleanWebpackPlugin()],
+  output: {
+    filename: '[name].js',
+    //动态导入
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
+}
